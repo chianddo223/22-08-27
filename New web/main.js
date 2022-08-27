f81e86dbc9d70973781e7fc0d985a4d6ed24cf10
chianddo223 <110188103+chianddo223@users.noreply.github.com>
2022-08-27 17:37:26 +0900

    Add files via upload

---
+++ New web/main.js
@@ -0,0 +1,10 @@
+
+
+const hambug = document.querySelector('.navbar_hambug')
+const items = document.querySelector('.navitems')
+const icons = document.querySelector('.navicons')
+
+hambug.addEventListener('click', () => {
+  items.classList.toggle('active');
+  icons.classList.toggle('active');
+});
